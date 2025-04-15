7712bf4ecc910090a307134c3e51635cb3a1ca36
Alok Gupta <enggalok@yahoo.com>
2025-04-15 09:34:00 -0500

    prd api intergrateion

--- src/app/bedrock.service.ts
+++ src/app/bedrock.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +8,14 @@ export class BedrockService {
   constructor(private http: HttpClient) {}
 
   generateText(prompt: string) {
-    return this.http.post<string>('https://ntypzyxmz5.execute-api.us-west-2.amazonaws.com/dev/alok-test-007', { prompt });
+    let params = new HttpParams()
+      .set('agent_id', 'HHH33W9BJO')
+      .set('alias_id', 'XISRC0Q5SN')
+      .set('session_id', '1234')
+      .set('prompt', prompt);
+
+      return this.http.post<string>('https://ntypzyxmz5.execute-api.us-west-2.amazonaws.com/dev/invoke-agent', { params: params });
+
+    //return this.http.post<string>('https://ntypzyxmz5.execute-api.us-west-2.amazonaws.com/dev/alok-test-007', { prompt });
   }
 }
