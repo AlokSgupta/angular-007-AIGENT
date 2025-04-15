e2dfb4a4ca661800f9ca0fb00b248d40a129a877
Alok Gupta <enggalok@yahoo.com>
2025-04-15 10:41:13 -0500

    dara as param

--- src/app/bedrock.service.ts
+++ src/app/bedrock.service.ts
@@ -8,13 +8,22 @@ export class BedrockService {
   constructor(private http: HttpClient) {}
 
   generateText(prompt: string) {
+    let agentid =  "HHH33W9BJO";
+    let aliasid = "XISRC0Q5SN";
     let params = new HttpParams()
-      .set("agent_id", "HHH33W9BJO")
-      .set("alias_id", "XISRC0Q5SN")
+      .set("agent_id", agentid)
+      .set("alias_id", aliasid)
       .set("session_id", "1234")
       .set("prompt", prompt);
 
-      return this.http.post<string>('https://ntypzyxmz5.execute-api.us-west-2.amazonaws.com/dev/invoke-agent', { params: params });
+      const data = {
+        agent_id: agentid,
+        alias_id: aliasid,
+        session_id: "1234",
+        prompt: prompt
+      }
+
+      return this.http.post<string>('https://ntypzyxmz5.execute-api.us-west-2.amazonaws.com/dev/invoke-agent', data);
 
     //return this.http.post<string>('https://ntypzyxmz5.execute-api.us-west-2.amazonaws.com/dev/alok-test-007', { prompt });
   }
